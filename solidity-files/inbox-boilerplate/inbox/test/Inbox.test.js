10cb6a07aa93f88b4f3f9bb9f0c05b10249028b1
Enveed <sopagnarithseam@gmail.com>
2024-01-22 17:18:31 +0700

    fix - setup running contract locally

--- solidity-files/inbox-boilerplate/inbox/test/Inbox.test.js
+++ solidity-files/inbox-boilerplate/inbox/test/Inbox.test.js
@@ -1,6 +1,26 @@
-const ganache = require('ganache');
-const { Web3 } = require('web3');
+const ganache = require("ganache");
+const assert = require("assert");
+const { Web3 } = require("web3");
+const web3 = new Web3(ganache.provider());
 
-// updated ganache and web3 imports added for convenience
+class Car {
+  park() {
+    return "Stopped";
+  }
 
-// contract test code will go here
+  drive() {
+    return "Vroom";
+  }
+}
+
+describe("Car", () => {
+  it("should return 'Stopped'", () => {
+    const car = new Car();
+    assert.equal(car.park(), "Stopped");
+  });
+
+  it("should return 'Vroom'", () => {
+    const car = new Car();
+    assert.equal(car.drive(), "Vroom");
+  });
+});
